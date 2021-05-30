7eda0f4ad5df1eb19ebf4451120f4ebf5f934e14
Subhamraj-Pathy <subhamrajpathy@gmail.com>
2021-05-30 11:55:19 +0530

    Hero Introduced

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import Head from 'next/head';
 import Image from 'next/image';
+import Link from 'next/link';
 import Nav from '../components/Navbar';
 import { getNewAdditions } from '../helpers/kicks';
 import Card from '../components/Card/card';
@@ -37,6 +38,41 @@ const Home = () => {
           {/* ----------------------- */}
 
 
+          {/* HERO */}
+          <div className='p-4 shadow-md mb-4 w-full h-auto space-y-6 flex flex-col items-center justify-center lg:flex-row lg:items-center lg:justify-between bg-black text-white'>
+            <div className='flex flex-col lg:flex-row'>
+              <img
+                loading='lazy'
+                src='/images/Hero.jpg'
+                alt='hero'
+              />
+              <div className='relative flex items-center justify-center font-bold text-8xl tracking-widest lg:transform lg:-rotate-90 lg:text-9xl lg:-m-40'>
+                NIKE
+              <div className='bg-black py-0.5 w-full absolute top-1/2 transform -translate-y-1/3 text-center font-thin text-2xl lg:text-3xl tracking-widest'>
+                  JUST DO IT
+              </div>
+              </div>
+            </div>
+            <div className='flex flex-col justify-center items-center lg:items-end'>
+              <p className='text-center tracking-widest font-thin text-2xl inline lg:hidden'>Discover the latest collection</p>
+              <p className='text-right tracking-widest font-thin text-4xl hidden lg:inline mb-6'>Discover the</p>
+              <p className='text-right tracking-widest font-thin text-4xl hidden lg:inline'>latest collection</p>
+              <Link href='/shop'><div className='mt-6 lg:mt-10 px-6 py-4 text-2xl tracking-widest bg-white text-black cursor-pointer'>Shop Now</div></Link>
+            </div>
+          </div>
+
+          {/* NEW ARRIVALS */}
+          <div className='text-center my-6 mt-14 tracking-widest font-light text-2xl lg:text-4xl'>
+            NEW ARRIVALS
+          </div>
+          <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3'>
+            {
+              newAdditions?.map((el, i) => (
+                <Card key={i} Item={el} />
+              ))
+            }
+          </div>
+
           {/* BANNER */}
           <div className='shadow-md w-full h-80 flex justify-center' style={{ background: '#FF314D' }}>
             <img
@@ -46,9 +82,10 @@ const Home = () => {
             />
           </div>
 
-          {/* NEW ARRIVALS */}
+          {/* BEST SELLER */}
+          {/* BEST SELLERS IS DUPLICATED AS OF NOW */}
           <div className='text-center my-6 tracking-widest font-light text-2xl lg:text-4xl'>
-            NEW ARRIVALS
+            BEST SELLERS
           </div>
           <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3'>
             {
@@ -65,19 +102,6 @@ const Home = () => {
               src='/images/mid.png'
               alt='banner'
             />
-          </div>
-
-          {/* BEST SELLER */}
-          {/* BEST SELLERS IS DUPLICATED AS OF NOW */}
-          <div className='text-center my-6 tracking-widest font-light text-2xl lg:text-4xl'>
-            BEST SELLERS
-          </div>
-          <div className='grid grid-flow-row-dense md:grid-cols-2 lg:grid-cols-3'>
-            {
-              newAdditions?.map((el, i) => (
-                <Card key={i} Item={el} />
-              ))
-            }
           </div>
 
         </div>
