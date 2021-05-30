c85e630a0c8aec93fb87ff3bb2ce0deee0e01dc3
Subhamraj-Pathy <subhamrajpathy@gmail.com>
2021-05-30 06:36:35 +0530

    Banner

--- pages/index.js
+++ pages/index.js
@@ -13,16 +13,24 @@ const Home = () => {
 
       <div className='w-screen max-w-screen-2xl shadow min-h-screen'>
         <Nav />
-        <div className='px-16 bg-gray-200 min-h-screen'>
-          <div className='float-right hidden lg:flex py-10'>
+        <div className='pt-16 pr-2 pl-2 lg:pt-6 lg:pr-8 lg:pl-36 bg-gray-200 min-h-screen'>
+          <div className='hidden lg:flex items-center justify-end w-full py-4'>
             <Image
               src='/images/Brand-min.png'
-              width={150}
-              height={60}
+              width={120}
+              height={50}
             />
           </div>
 
           {/* REST CONTENT GOES BELOW */}
+
+          <div className='shadow-md w-full h-80 flex justify-center' style={{ background: '#FF314D' }}>
+            <img
+              loading='lazy'
+              src='/images/BannerImg.png'
+              alt='banner'
+            />
+          </div>
         </div>
       </div>
     </div>
